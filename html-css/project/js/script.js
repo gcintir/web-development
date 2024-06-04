1210456177fa3c94095a5fe32ab81dd7e0e769cf
DIR\guray.cintir <guray.cintir@accenture.com>
2024-06-04 23:29:22 +0300

    contact flow

--- html-css/project/js/script.js
+++ html-css/project/js/script.js
@@ -10,3 +10,7 @@ navEl.addEventListener('click', () => {
     navEl.classList.remove('nav--open');
     hamburgerEl.classList.remove('hamburger--open');
 });
+
+function sendEmail (name, email, subject, message) {
+    window.location.href ="mailto:halilibrahimozhanli@gmail.com?subject=" + subject + "&body=" + message + "%0D%0A%0D%0ARegards,%0D%0A" + name + "%0D%0A" + email;
+}
