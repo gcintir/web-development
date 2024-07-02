52ab4c574d9c6062234e71d64ab43e58fb731b39
DIR\guray.cintir <guray.cintir@accenture.com>
2024-07-02 16:51:27 +0300

    checkout page exercise

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -63,13 +63,15 @@ products.forEach((product) => {
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
-function updateCartQuantity() {
-  let cartQuantity = 0;
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
+updateCartQuantityInfo();
 
-  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+function updateCartQuantityInfo() {
+  let cartQuantity = calculateCartQuantity();
+  if (cartQuantity === 0) {
+    document.querySelector(".js-cart-quantity").innerHTML = '';
+  } else {
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  }
 }
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
@@ -77,6 +79,6 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     const productId = button.dataset.productId;
 
     addToCart(productId);
-    updateCartQuantity();
+    updateCartQuantityInfo();
   });
 });
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,7 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
@@ -37,9 +38,19 @@ cart.forEach((cartItem) => {
                   <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
-                  <span class="update-quantity-link link-primary">
+                  <span class="update-quantity-link link-primary
+                    js-update-link" data-product-id="${matchingProduct.id}">
                     Update
                   </span>
+
+                  <input class="quantity-input 
+                  js-quantity-input-${matchingProduct.id}">
+                  </input>
+                  <span class="save-quantity-link link-primary
+                     js-save-link" data-product-id="${matchingProduct.id}">
+                    Save
+                  </span>
+
                   <span class="delete-quantity-link link-primary
                     js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
@@ -105,5 +116,36 @@ document.querySelectorAll('.js-delete-link')
         removeFromCart(productId);
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         container.remove();
+        updateCheckoutQuantityInfo();
     });
 });
+
+document.querySelectorAll('.js-update-link')
+.forEach(link => {
+    link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.classList.add('is-editing-quantity');
+    });
+});
+
+document.querySelectorAll('.js-save-link')
+.forEach(link => {
+    link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.classList.remove('is-editing-quantity');
+        const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
+        updateQuantity(productId, Number(quantityInput.value));
+        updateCheckoutQuantityInfo();
+        quantityInput.value = '';
+    });
+});
+
+updateCheckoutQuantityInfo();
+
+function updateCheckoutQuantityInfo () {
+  let cartQuantity = calculateCartQuantity();
+  document.querySelector(".js-checkout-quantity")
+  .innerHTML = `Checkout (<a class="return-to-home-link"href="amazon.html">${cartQuantity} items</a>)`;
+}
