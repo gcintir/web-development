9606764b6a65b7cd637af0e6ded979ed849d476f
DIR\guray.cintir <guray.cintir@accenture.com>
2024-07-09 08:32:10 +0300

    order summary page

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -5,11 +5,13 @@ if (!cart) {
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
+      deliveryOptionId: '1'
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 1,
-    },
+      deliveryOptionId: '2'
+    }
   ];
 }
 
@@ -32,6 +34,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: '1'
     });
   }
 
@@ -71,4 +74,18 @@ export function calculateCartQuantity() {
     cartQuantity += cartItem.quantity;
   });
   return cartQuantity;
+}
+
+
+export function updateDeliveryOption (productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
 }
